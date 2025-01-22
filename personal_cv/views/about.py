c7ccee156a7d0e9bcfcf52c441a5c87c0c2b1b19
a.merkulov <a.merkulov@spider.ru>
2025-01-22 13:48:37 +0300

    Hotfixes

--- personal_cv/views/about.py
+++ personal_cv/views/about.py
@@ -5,7 +5,7 @@ from personal_cv.utils import display_ed, get_hard_skills
 
 col1, col2 = st.columns(2, gap='small', vertical_alignment='center')
 with col1:
-    st.image(PROFILE_PIC, use_column_width=True, width=300)
+    st.image(PROFILE_PIC, use_container_width=True, width=300)
 
 with col2:
     st.title(NAME, anchor=False)
